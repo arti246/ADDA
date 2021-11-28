fb291be8ef3896db11ddd786ee4b533cfbbba2c3
artur <artur_grishin_12345@mail.ru>
2021-11-28 14:33:08 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace WorkWithDataBase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Add = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.database1DataSet = new WorkWithDataBase.Database1DataSet();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button_Update = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_Add
+            // 
+            this.button_Add.Location = new System.Drawing.Point(296, 9);
+            this.button_Add.Name = "button_Add";
+            this.button_Add.Size = new System.Drawing.Size(75, 23);
+            this.button_Add.TabIndex = 0;
+            this.button_Add.Text = "Добавить";
+            this.button_Add.UseVisualStyleBackColor = true;
+            this.button_Add.Click += new System.EventHandler(this.button_Add_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите фамилии обучающихся:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(190, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(701, 296);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 41);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(158, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Среднее значение оценок";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button_Update
+            // 
+            this.button_Update.Location = new System.Drawing.Point(408, 9);
+            this.button_Update.Name = "button_Update";
+            this.button_Update.Size = new System.Drawing.Size(109, 23);
+            this.button_Update.TabIndex = 9;
+            this.button_Update.Text = "Обновить оценки";
+            this.button_Update.UseVisualStyleBackColor = true;
+            this.button_Update.Click += new System.EventHandler(this.button_Update_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(523, 12);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(708, 374);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button_Update);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Add);
+            this.Name = "Form1";
+            this.Text = "WorkWithDataBase";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Add;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button_Update;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WorkWithDataBase
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlConnection;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            string ConnetcionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\3 курс\ПП\ПРсSQL\WorkWithDataBase\WorkWithDataBase\Database1.mdf;Integrated Security=True";
+
+            sqlConnection = new SqlConnection(ConnetcionString);
+
+            await sqlConnection.OpenAsync();
+            SqlDataReader SqlReader = null;
+            SqlCommand comand = new SqlCommand("SELECT * FROM [Table]", sqlConnection);
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM [Table]", sqlConnection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+
+            try
+            {
+                SqlReader = await comand.ExecuteReaderAsync();
+
+                
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (SqlReader != null)
+                    SqlReader.Close();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (sqlConnection != null && sqlConnection.State != ConnectionState.Closed)
+                sqlConnection.Close();
+        }
+
+        private async void button_Add_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                SqlCommand comand = new SqlCommand("INSERT INTO [Table] (FAM, Mathematics, Biology, Programming, Chemistry, Physics) VALUES (@FAM, @Mathematics, @Biology, @Programming, @Chemistry, @Physics)", sqlConnection);
+
+                try
+                {
+                    Random random = new Random();
+                    comand.Parameters.AddWithValue("FAM", textBox1.Text);
+                    comand.Parameters.AddWithValue("Mathematics", Convert.ToString(random.Next(2, 6)));
+                    comand.Parameters.AddWithValue("Biology", Convert.ToString(random.Next(2, 6)));
+                    comand.Parameters.AddWithValue("Programming", Convert.ToString(random.Next(2, 6)));
+                    comand.Parameters.AddWithValue("Chemistry", Convert.ToString(random.Next(2, 6)));
+                    comand.Parameters.AddWithValue("Physics", Convert.ToString(random.Next(2, 6)));
+
+                    await comand.ExecuteNonQueryAsync();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите текст", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            string ConnetcionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\3 курс\ПП\ПРсSQL\WorkWithDataBase\WorkWithDataBase\Database1.mdf;Integrated Security=True";
+            sqlConnection = new SqlConnection(ConnetcionString);
+            sqlConnection.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM [Table]", sqlConnection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+
+            textBox1.Text = "";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            string ConnetcionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\3 курс\ПП\ПРсSQL\WorkWithDataBase\WorkWithDataBase\Database1.mdf;Integrated Security=True";
+            sqlConnection = new SqlConnection(ConnetcionString);
+
+            //Удаление столбца
+            if (checkBox1.Checked == false)
+            {
+                try
+                {
+                    string alter = "ALTER TABLE [Table] DROP COLUMN AverageScore";
+                    sqlConnection.Open();
+                    SqlCommand cmd = sqlConnection.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = alter;
+                    cmd.ExecuteNonQuery();
+
+                    SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM [Table]", sqlConnection);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    dataGridView1.DataSource = table;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            //Добавление столбца
+            if (checkBox1.Checked == true)
+            {
+                try
+                {
+                    //Добавление столбца
+                    string alter = "ALTER TABLE [Table] ADD AverageScore INT";
+                    sqlConnection.Open();
+                    SqlCommand cmd = sqlConnection.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = alter;
+                    cmd.ExecuteNonQuery();
+
+                    //Заполнение нового столбца
+                    AverageScoreUpdate();
+
+                    SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM [Table]", sqlConnection);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    dataGridView1.DataSource = table;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        public void AverageScoreUpdate()
+        {
+            try
+            {
+                string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\3 курс\ПП\ПРсSQL\WorkWithDataBase\WorkWithDataBase\Database1.mdf;Integrated Security=True";
+
+                string sqlExpression = "SELECT * FROM [Table]";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(sqlExpression, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows) // если есть данные
+                    {
+                        while (reader.Read()) // построчно считываем данные
+                        {
+                            object FAM = reader.GetValue(0);
+                            object Mathematics = reader.GetValue(1);
+                            object Biology = reader.GetValue(2);
+                            object Programming = reader.GetValue(3);
+                            object Chemistry = reader.GetValue(4);
+                            object Physics = reader.GetValue(5);
+
+                            int sum = Convert.ToInt32(Mathematics) + Convert.ToInt32(Biology) + Convert.ToInt32(Programming) + Convert.ToInt32(Chemistry) + Convert.ToInt32(Physics);
+
+                            SqlCommand comandInsert = new SqlCommand("UPDATE [Table] SET [AverageScore] = " + sum + " WHERE [AverageScore] IS NULL AND [FAM] = \'" + FAM + "\'", sqlConnection);
+
+                            comandInsert.ExecuteNonQuery();
+                        }
+                    }
+                    reader.Close();
+                }
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM [Table]", sqlConnection);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void button_Update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Random random = new Random();
+
+                string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\3 курс\ПП\ПРсSQL\WorkWithDataBase\WorkWithDataBase\Database1.mdf;Integrated Security=True";
+
+                string sqlExpression = "SELECT * FROM [Table]";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    sqlConnection.Open();
+                    SqlCommand command = new SqlCommand(sqlExpression, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows) // если есть данные
+                    {
+                        while (reader.Read()) // построчно считываем данные
+                        {
+                            object FAM = reader.GetValue(0);
+                            int Mathematics = random.Next(2, 6);
+                            int Biology = random.Next(2, 6);
+                            int Programming = random.Next(2, 6);
+                            int Chemistry = random.Next(2, 6);
+                            int Physics = random.Next(2, 6);
+
+                            SqlCommand comandUpdate = new SqlCommand("UPDATE [Table] SET [Mathematics] = " + Mathematics + ", [Biology] = " + Biology + ", [Programming] = " + Programming + ", [Chemistry] = " + Chemistry + ", [Physics] = " + Physics + " WHERE [FAM] = \'" + FAM + "\'", sqlConnection);
+
+                            int sum = Mathematics + Biology + Programming + Chemistry + Physics;
+                            SqlCommand comandInsert = new SqlCommand("UPDATE [Table] SET [AverageScore#2] = " + sum + " WHERE [AverageScore#2] IS NULL AND [FAM] = \'" + FAM + "\'", sqlConnection);
+
+                            comandUpdate.ExecuteNonQuery();
+                            comandInsert.ExecuteNonQuery();
+                        }
+                    }
+                    connection.Close();
+                    reader.Close();
+                }
+
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM [Table]", sqlConnection);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string ConnetcionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\3 курс\ПП\ПРсSQL\WorkWithDataBase\WorkWithDataBase\Database1.mdf;Integrated Security=True";
+                sqlConnection = new SqlConnection(ConnetcionString);
+                sqlConnection.Open();
+
+                if (checkBox2.Checked == true)
+                {
+                    //Добавление столбца
+                    string alter = "ALTER TABLE [Table] ADD [AverageScore#2] INT";
+                    SqlCommand cmd = sqlConnection.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = alter;
+                    cmd.ExecuteNonQuery();
+                }
+                if (checkBox2.Checked == false)
+                {
+                    string alter = "ALTER TABLE [Table] DROP COLUMN [AverageScore#2]";
+                    SqlCommand cmd = sqlConnection.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = alter;
+                    cmd.ExecuteNonQuery();
+                }
+
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM [Table]", sqlConnection);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
